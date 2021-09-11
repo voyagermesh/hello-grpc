651ecd80f739a61c3445c20213ad29d08b2057b7
Tamal Saha <tamal@appscode.com>
2021-09-11 10:53:30 -0700

    Add license header
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/apis/hello/v1alpha1/doc.go
+++ pkg/apis/hello/v1alpha1/doc.go
@@ -14,30 +14,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
-
-import (
-	"fmt"
-	"log"
-	"os"
-
-	"gomodules.xyz/runtime"
-	"voyagermesh.dev/hello-grpc/pkg/cmds"
-	"github.com/spf13/cobra/doc"
-)
-
-// ref: https://github.com/spf13/cobra/blob/master/doc/md_docs.md
-func main() {
-	rootCmd := cmds.NewRootCmd("")
-	dir := runtime.GOPath() + "/src/voyagermesh.dev/hello-grpc/docs/reference"
-	fmt.Printf("Generating cli markdown tree in: %v\n", dir)
-	err := os.RemoveAll(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = os.MkdirAll(dir, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-	doc.GenMarkdownTree(rootCmd, dir)
-}
+package v1alpha1
